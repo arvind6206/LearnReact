8860246235478bb0debc530db474215162e64d72
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-05 08:39:02 +0530

    customHooks

---
+++ learnReact/CustomHooks/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function useCounter(){
+  const [count, setCount] = useState(0)
+  function increaseCount(){
+    setCount(count + 1)
+  }
+  return {
+    count: count,
+    increaseCount: increaseCount
+  }
+}
+
+function App() {
+  const {count, increaseCount} = useCounter()
+  return (
+    <div>
+      <button onClick={increaseCount}>Increase {count}</button>
+    </div>
+  )
+}
+
+export default App
