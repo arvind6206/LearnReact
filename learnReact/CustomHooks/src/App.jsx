422b8b3e8540e5d3536018b2e9baaf5bdd66e2f0
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-05 08:56:24 +0530

    hooks

--- learnReact/CustomHooks/src/App.jsx
+++ learnReact/CustomHooks/src/App.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-function useCounter(){
-  const [count, setCount] = useState(0)
-  function increaseCount(){
-    setCount(count + 1)
-  }
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-}
 
 function App() {
-  const {count, increaseCount} = useCounter()
+  const [post, setPost] = useState({})
+
+  async function getPosts(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const json = await response.json()
+    setPost(json)
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <div>
-      <button onClick={increaseCount}>Increase {count}</button>
+      {post.title}
     </div>
   )
 }
+
+
 
 export default App
