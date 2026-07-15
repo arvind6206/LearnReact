c8777dfa61f9ba4383b38c2cc79af78bf9869129
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-15 14:05:41 +0530

    Digital Clock

---
+++ Digital Clock/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+
+  // const currDate = new Date()
+  
+  return (
+  
+    <div>
+      <h1>Digital Clock</h1>
+      <h2>{Date()}</h2>
+    </div>
+  )
+}
+
+export default App
