210c5e6cf09024e4ae40884de526bc3a98e796a7
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-15 11:50:15 +0530

    button style

--- Calculator/src/components/ButtonGrid.jsx
+++ Calculator/src/components/ButtonGrid.jsx
@@ -10,8 +10,22 @@ function ButtonGrid() {
 ];
   return (
     <div className='grid grid-cols-4 gap-2'>
-     {buttons.map((btn)=>{
-       return <button key={i}>{btn}</button>
+     {buttons.map((btn, i)=>{
+       return <button
+       className={`
+  w-16 aspect-square rounded-xl font-semibold text-xl
+  transition cursor-pointer hover:scale-95
+  ${btn === "=" ? "bg-blue-500 text-white" : btn === "AC" || btn === "DEL" ? 
+    "bg-red-500 text-white" : ["+", "-", "*", "/", "%"].includes(btn) ? 
+    "bg-orange-500 text-white" : "bg-white text-black hover: bg-gray-200"
+    }
+
+`}
+       
+    key={i}>
+        {btn}
+    </button>
+    
 })}
       
     </div>
