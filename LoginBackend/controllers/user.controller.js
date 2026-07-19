3b0917f5c2342671363f5705bcbb9716ff4d3ba1
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-19 13:09:14 +0530

    zod

--- LoginBackend/controllers/user.controller.js
+++ LoginBackend/controllers/user.controller.js
@@ -1,7 +1,33 @@
 import { UserModel } from "../models/User.js"
+import bcrypt from 'bcryptjs'
+import * as z from 'zod'
 
 export const signup = async(req, res) => {
+
+    const registeredUserSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string()
+        .min(6, "Password must be at least 6 characters")
+        .refine((val) => /[A-Z]/.test(val), {
+            message: "Password must contain at least one uppercase letter"
+        })
+        .refine((val) => /[0-9]/.test(val), {
+            message: "Password must contain at least one number"
+        })
+        .refine((val) => /[@$!%*#?&]/.test(val), {
+            message: "Password must contain at least one special character"
+        })
+})
     try {
+        const result = registeredUserSchema.safeParse(req.body)
+        if(!result.success){
+            return res.status(400).json({
+                msg: "Input Validation error",
+                errors: result.error.issues
+            })
+        }
+
         const {name, email, password} = req.body
 
         if(!email || !password || !email){
