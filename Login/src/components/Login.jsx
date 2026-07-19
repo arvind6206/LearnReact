f8cbe83c541942f2c51b0288722b9412708c6a07
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-19 10:59:05 +0530

    Latest

--- Login/src/components/Login.jsx
+++ Login/src/components/Login.jsx
@@ -2,19 +2,38 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="max-h-50 w-sm bg-blue-600">
-        <div className='flex justify-center text-2xl font-bold'>
+    <div className="bg-gray-50 flex items-center justify-center min-h-screen">
+      <div className="bg-white rounded-2xl shadow-lg p-8 w-full max-w-sm">
+        <div className="flex justify-center text-2xl font-bold">
           <h1>Welcome Back!</h1>
         </div>
-        <div className='m-5'>
+        <div className="m-5">
           <label>Email Address</label>
-          <br/>
-          <input type="email" placeholder="Enter your email" />
-          <br/>
+          <br />
+          <input
+            className="w-75 h-9 rounded-sm mt-1 mb-2 p-2"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <br />
           <label>Password</label>
-          <br/>
-          <input type="password" placeholder="Enter your password" />
+          <br />
+          <input
+            className="w-75 h-9 rounded-sm mt-1 mb-2 p-2"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <p className="text-sm">Forgot Password?</p>
+        </div>
+        <div className="flex justify-between m-5 text-sm">
+          <div className="flex flex-start">
+            <input type="checkbox" />
+            <p>Remember me</p>
+          </div>
+          <p className='text-indigo-700'>Create Account</p>
+        </div>
+        <div className='bg-indigo-600 h-9 flex items-center justify-center rounded-sm ml-4'>
+          <button className='text-white'>Login</button>
         </div>
       </div>
     </div>
