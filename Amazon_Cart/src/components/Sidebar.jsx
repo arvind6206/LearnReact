8e3060978023430288f8fbf793533f601982057a
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-20 14:21:55 +0530

    sidebar

---
+++ Amazon_Cart/src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import WishlistTopbar from "./WishlistTopbar";
+
+function Wishlist() {
+  return (
+    <div className="flex min-h-screen">
+      
+      {/* Sidebar */}
+      <div className="w-64 bg-slate-200 p-5">
+        <div className='w-55 bg-slate-300'>
+          <h1 className="text-3xl font-bold">Your Wish List</h1>
+        <span className="text-md font-semibold">Default List</span>
+        </div>
+      </div>
+
+      {/* Right Content */}
+      <div className="flex-1 p-5">
+        {/* Wishlist Header */}
+        <h2 className="text-2xl font-semibold">
+          <WishlistTopbar/>
+        </h2>
+
+        {/* Toolbar */}
+
+        {/* Product Grid */}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Wishlist;
---
+++ Amazon_Cart/src/components/WishlistTopbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WishlistTopbar() {
+  return (
+    <div>
+      wISHlISTt OPbAR
+    </div>
+  )
+}
+
+export default WishlistTopbar
